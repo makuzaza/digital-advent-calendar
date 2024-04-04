98d58594d8204d23c1c3127cec4bbf8454b2271f
Mithun Munsi <mithun.munsi@gmail.com>
2024-04-04 13:39:33 +0300

    Time and order feature added

---
+++ frontend/src/components/DateAndOrderPicker.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import './../index.css';
+
+interface Order {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  orders: Order[];
+}
+
+const DateAndOrderPicker: React.FC<Props> = ({ orders }) => {
+  const [fromDate, setFromDate] = useState<string>('');
+  const [toDate, setToDate] = useState<string>('');
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+
+  const handleFromDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFromDate(event.target.value);
+  };
+
+  const handleToDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToDate(event.target.value);
+  };
+
+  const handleOrderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const orderId = parseInt(event.target.value);
+    const order = orders.find((order) => order.id === orderId);
+    setSelectedOrder(order || null);
+  };
+
+  return (
+    <div>
+      <div>
+        <label
+          htmlFor="fromDatePicker"
+          style={{ display: 'inline-block', width: '100px' }}
+        >
+          From Date:
+        </label>
+        <input
+          className="input"
+          type="date"
+          id="fromDatePicker"
+          value={fromDate}
+          onChange={handleFromDateChange}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="toDatePicker"
+          style={{ display: 'inline-block', width: '100px', margin: '10px 0' }}
+        >
+          To Date:
+        </label>
+        <input
+          type="date"
+          id="toDatePicker"
+          value={toDate}
+          onChange={handleToDateChange}
+        />
+      </div>
+
+      <label className="label" htmlFor="orderPicker">
+        Select an Order:
+      </label>
+      <select
+        id="orderPicker"
+        value={selectedOrder ? selectedOrder.id : ''}
+        onChange={handleOrderChange}
+      >
+        <option value="">Select an order...</option>
+        {orders.map((order) => (
+          <option key={order.id} value={order.id}>
+            {order.name}
+          </option>
+        ))}
+      </select>
+
+      <div>
+        <h2 style={{ fontSize: '1.3rem' }}>From Date: {fromDate}</h2>
+        <h2 style={{ fontSize: '1.3rem' }}>To Date: {toDate}</h2>
+        {selectedOrder && (
+          <h2 style={{ fontSize: '1.3rem' }}>
+            Selected Order: {selectedOrder.name}
+          </h2>
+        )}
+      </div>
+      <div className="buttons">
+        <button className="btn" type="submit">
+          Reset
+        </button>
+        <button className="btn" type="submit">
+          Apply
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DateAndOrderPicker;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,54 +1,55 @@
-import React, { useState } from "react";
-import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
-import AudiotrackIcon from "@mui/icons-material/Audiotrack";
-import SidebarSounds from "./SidebarSounds/SidebarSounds";
+import React, { useState } from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import SidebarSounds from './SidebarSounds/SidebarSounds';
 import RttIcon from '@mui/icons-material/Rtt';
-import Texts from "./Texts";
+import Texts from './Texts';
+import DateAndOrderPicker from './DateAndOrderPicker';
 
 const drawerWidth = 220;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up('sm')]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
@@ -59,17 +60,17 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })<AppBarProps>(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
+  transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -77,25 +78,25 @@ const AppBar = styled(MuiAppBar, {
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
   ...(open && {
     ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
   }),
 }));
 
 type Props = {
-  title : string;
-  subtitle : string;
+  title: string;
+  subtitle: string;
   setTitle: (title: string) => void;
   setSubtitle: (subtitle: string) => void;
   titleFont: string;
@@ -112,7 +113,24 @@ type Props = {
   subtitleColor: string;
 };
 
-const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, titleFont, titleFontSize, subtitleFont, subTitleFontSize, setTitleFont, setSubtitleFont, setTitleFontSize, setSubTitleFontSize, titleColor, subtitleColor, setTitleColor, setSubtitleColor } ) => {
+const Sidebar: React.FC<Props> = ({
+  title,
+  subtitle,
+  setTitle,
+  setSubtitle,
+  titleFont,
+  titleFontSize,
+  subtitleFont,
+  subTitleFontSize,
+  setTitleFont,
+  setSubtitleFont,
+  setTitleFontSize,
+  setSubTitleFontSize,
+  titleColor,
+  subtitleColor,
+  setTitleColor,
+  setSubtitleColor,
+}) => {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
   const [slideIndex, setSlideIndex] = useState<number | null>(null);
@@ -149,10 +167,10 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
-        <Toolbar sx={{ bgcolor: "#d985af" }}>
+        <Toolbar sx={{ bgcolor: '#d985af' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -160,7 +178,7 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
             edge="start"
             sx={{
               marginRight: 5,
-              ...(open && { display: "none" }),
+              ...(open && { display: 'none' }),
             }}
           >
             <MenuIcon />
@@ -170,7 +188,7 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
       <Drawer variant="permanent" open={open}>
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
+            {theme.direction === 'rtl' ? (
               <ChevronRightIcon />
             ) : (
               <ChevronLeftIcon />
@@ -178,28 +196,28 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <List sx={{ bgcolor: "#968382", height: 760, color: "#fff" }}>
-          {["Time and Order", "Text", "Upload Image", "Sounds"].map(
+        <List sx={{ bgcolor: '#968382', height: 760, color: '#fff' }}>
+          {['Time and Order', 'Text', 'Upload Image', 'Sounds'].map(
             (text, index) => (
               <ListItem
                 onClick={() => toggleSlideOpen(index)}
                 key={text}
                 disablePadding
-                sx={{ display: "block" }}
+                sx={{ display: 'block' }}
               >
                 <ListItemButton
                   sx={{
                     minHeight: 48,
-                    justifyContent: open ? "initial" : "center",
+                    justifyContent: open ? 'initial' : 'center',
                     px: 2.5,
                   }}
                 >
                   <ListItemIcon
                     sx={{
                       minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
-                      color: "#fff",
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                      color: '#fff',
                     }}
                   >
                     {index === 0 && <HourglassEmptyIcon />}
@@ -219,13 +237,11 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
         <Box
           component="main"
           p={1}
-          sx={{ width: 250, height: 760, bgcolor: "#534342", color: "#fff" }}
+          sx={{ width: 250, height: 760, bgcolor: '#534342', color: '#fff' }}
         >
           {slideIndex === 0 && (
             /* This is the place for the first icons component etc...*/
-            <Typography paragraph>
-              This is the slide for the icon #{slideIndex + 1}
-            </Typography>
+            <DateAndOrderPicker orders={[]} />
           )}
           {slideIndex === 1 && (
             <Texts
@@ -259,6 +275,6 @@ const Sidebar: React.FC<Props>  = ( { title, subtitle, setTitle, setSubtitle, ti
       )}
     </Box>
   );
-}
+};
 
 export default Sidebar;
