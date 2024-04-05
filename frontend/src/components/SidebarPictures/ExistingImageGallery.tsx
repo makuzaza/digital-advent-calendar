489a672ab64fe6fac46afa6eb4e661b5e87acfac
AnikaTahashin10 <anikatahashin90@gmail.com>
2024-04-05 07:59:51 +0300

    testing the bg with a url

--- frontend/src/components/SidebarPictures/ExistingImageGallery.tsx
+++ frontend/src/components/SidebarPictures/ExistingImageGallery.tsx
@@ -8,24 +8,24 @@ const CalendarEditor = () => {
   const [randomImages, setRandomImages] = useState<string[]>([]);
   const [selectedBackground, setSelectedBackground] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchRandomImages = async () => {
-      try {
-        const response = await axios.get('https://api.unsplash.com/photos/random', {
-          params: {
-            count: 8, 
-            client_id: 'A9wMU_lZC4OW9kPTBjQOl6fncG6cTE13hDUtzDZ6xYE', 
-          },
-        });
-        const imageUrls = response.data.map((photo: any) => photo.urls.small);
-        setRandomImages(imageUrls);
-      } catch (error) {
-        console.error('Error fetching random images:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchRandomImages = async () => {
+  //     try {
+  //       const response = await axios.get('https://api.unsplash.com/photos/random', {
+  //         params: {
+  //           count: 8, 
+  //           client_id: 'A9wMU_lZC4OW9kPTBjQOl6fncG6cTE13hDUtzDZ6xYE', 
+  //         },
+  //       });
+  //       const imageUrls = response.data.map((photo: any) => photo.urls.small);
+  //       setRandomImages(imageUrls);
+  //     } catch (error) {
+  //       console.error('Error fetching random images:', error);
+  //     }
+  //   };
 
-    fetchRandomImages();
-  }, []);
+  //   fetchRandomImages();
+  // }, []);
 
   const handleImageClick = (imageUrl: string) => {
     setSelectedBackground(imageUrl);
