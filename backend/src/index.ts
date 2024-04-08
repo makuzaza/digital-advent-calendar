97ce7977d3488c3f7602c29bd799a25e467d25c1
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-04-08 16:38:44 +0300

    create the basis for the backend

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to YODA Calendars™️ API 🌟 Your Online Digital Advent Calendars!"
+  );
+});
+
+// this route takes in the singup data and returns the user object
+// this is a mock route, in a real-world scenario, you would create a user in the firebase auth
+app.post("/signup", async (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    emailVerified: false,
+    disabled: false,
+  };
+
+  res.json(user);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
