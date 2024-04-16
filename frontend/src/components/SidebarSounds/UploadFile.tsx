bfaa2622b0bbf87fef1bcd80c23abaaa66c2c7ec
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-04-16 16:58:21 +0300

    fix and clean the code for UploadFile (music)

--- frontend/src/components/SidebarSounds/UploadFile.tsx
+++ frontend/src/components/SidebarSounds/UploadFile.tsx
@@ -2,55 +2,44 @@ import axios from "axios";
 
 // icons
 import UploadIcon from "@mui/icons-material/Upload";
-import { useState } from "react";
 
 type Props = {
   soundType: string;
 };
 
 const UploadFile: React.FC<Props> = ({ soundType }) => {
-  const [type, setType] = useState("");
-
   const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let musicType = type;
-
-    if (type === "") {
-      musicType = "soundFx";
-    }
-
     if (!e.target.files) return;
 
     const file = e.target.files[0];
     const formData = new FormData();
 
-    formData.append(musicType, file);
+    formData.append(soundType, file);
     axios
-      .post(`http://localhost:8000/storage/sounds/${musicType}`, formData)
+      .post(`http://localhost:8000/storage/sounds/${soundType}`, formData)
       .then((response) => {
-        console.log(`${musicType}:`, response.data);
+        console.log(`${soundType}:`, response.data);
       })
       .catch((error) => {
-        console.error(`Error uploading ${musicType}:`, error);
+        console.error(`Error uploading ${soundType}:`, error);
       });
-    return;
   };
 
   return (
     <div className="add-music">
-      <label htmlFor="upload" onClick={() => setType(soundType)}>
+      <label>
         <div className="btn">
           <h3>Upload</h3>
           <UploadIcon />
           <p>{soundType}</p>
         </div>
+        <input
+          type="file"
+          name="music"
+          onChange={handleUpload}
+          style={{ display: "none" }}
+        />
       </label>
-      <input
-        type="file"
-        name="music"
-        id="upload"
-        onChange={handleUpload}
-        style={{ display: "none" }}
-      />
     </div>
   );
 };
