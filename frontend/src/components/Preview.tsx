b8f39c311da9d58babc797ee69a04f7df80ac44c
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-04-29 10:29:48 +0300

    token check, fix imageUrl -> imageURL

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -66,7 +66,7 @@ interface Json {
     soundFxName: string;
   };
   image: {
-    imageUrl: string;
+    imageURL: string;
     uploadedImageName: string;
   };
   windowContent: WindowContent[];
@@ -95,7 +95,7 @@ const Preview: React.FC<Props> = ({
   const [openPreviewModal, setOpenPreviewModal] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [windowContent, setWindowContent] = useState<WindowContent[]>(
-    Array(windows.length).fill({ imageUrl: "", videoURL: "", text: "" })
+    Array(windows.length).fill({ imageURL: "", videoURL: "", text: "" })
   );
 
   const token = useAppSelector((state) => state.token.token);
@@ -130,7 +130,7 @@ const Preview: React.FC<Props> = ({
         soundFxName: musicFX,
       },
       image: {
-        imageUrl: selectedBackground,
+        imageURL: selectedBackground,
         uploadedImageName: uploadedImageName,
       },
       windowContent: windowContent,
