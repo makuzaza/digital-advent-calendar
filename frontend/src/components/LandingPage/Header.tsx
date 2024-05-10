1469c2b74c871a54f23e421d9da5f902fdf30c0e
Mithun Munsi <mithun.munsi@gmail.com>
2024-05-10 15:24:25 +0600

    added dropdown menu under profile icon

--- frontend/src/components/LandingPage/Header.tsx
+++ frontend/src/components/LandingPage/Header.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { AppBar, Toolbar, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Menu,
+  MenuItem,
+  IconButton,
+} from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./headerStyles.module.css";
 import Logout from "./Logout";
@@ -7,26 +14,33 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../auth/firebase";
 import { useAppSelector } from "../../hooks/useAppDispatch";
 import HamburgerMenu from "../Hamburger";
+import { AccountCircle } from "@mui/icons-material";
 
 const Header: React.FC = () => {
   const [user] = useAuthState(auth);
   const token = useAppSelector((state) => state.token.token);
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const menuItems = [
     { text: "Home", link: "/" },
-    { text: "Browse Calendars", link: "/calendars" },
-    { text: "Create Calendar", link: "/panel" },
+    { text: "Browse", link: "/calendars" },
+    { text: "Create", link: "/panel" },
     { text: "Favourites", link: "/favourites" },
     { text: "About", link: "/about" },
-    { text: "Admin", link: "/admin" },
-    
   ];
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   const mobileView = window.matchMedia("(max-width: 480px)").matches;
 
   return (
-
     <>
       <AppBar position="static" sx={{ backgroundColor: "#10617a" }}>
         <Toolbar>
@@ -43,19 +57,30 @@ const Header: React.FC = () => {
           </div>
           <div className={styles.rightAlign}>
             {token ? (
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  cursor: "pointer",
-                }}
-              >
-                <span onClick={() => navigate("/userinfo")}>
-                  Hello, {user?.displayName ? user?.displayName : user?.email}
-                </span>
-                <div style={{ marginRight: "20px", marginLeft: "20px" }}>
-                  <Logout />
-                </div>
+              <div>
+                <IconButton
+                  onClick={handleMenuOpen}
+                  color="inherit"
+                  aria-controls="menu"
+                  aria-haspopup="true"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                >
+                  <MenuItem onClick={() => navigate("/userinfo")}>
+                    Hello, {user?.displayName ? user?.displayName : user?.email}
+                  </MenuItem>
+                  <MenuItem onClick={() => navigate("/admin")}>Admin</MenuItem>
+                  <MenuItem onClick={handleMenuClose}>
+                    <Logout />
+                  </MenuItem>
+                </Menu>
               </div>
             ) : (
               <>
