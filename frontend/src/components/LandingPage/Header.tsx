434bd3649c3af20d940d5f525281f2947961fdf4
Makuzaza <maria.kuznetsova@edu.bc.fi>
2024-05-10 15:47:44 +0300

    dynamically changed Header, menuIcon

--- frontend/src/components/LandingPage/Header.tsx
+++ frontend/src/components/LandingPage/Header.tsx
@@ -1,38 +1,89 @@
 import React from "react";
-import { AppBar, Toolbar, Button } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, Button, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import styles from "./headerStyles.module.css";
 import Logout from "./Logout";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../auth/firebase";
 import { useAppSelector } from "../../hooks/useAppDispatch";
-import HamburgerMenu from "../Hamburger";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState, useEffect } from "react";
 
 const Header: React.FC = () => {
   const [user] = useAuthState(auth);
   const token = useAppSelector((state) => state.token.token);
   const navigate = useNavigate();
+  const location = useLocation();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   const menuItems = [
     { text: "Home", link: "/" },
-    { text: "Browse Calendars", link: "/calendars" },
-    { text: "Create Calendar", link: "/panel" },
+    { text: "Browse", link: "/calendars" },
+    { text: "Create", link: "/panel" },
     { text: "Favourites", link: "/favourites" },
     { text: "About", link: "/about" },
     { text: "Admin", link: "/admin" },
-    
   ];
 
-  const mobileView = window.matchMedia("(max-width: 480px)").matches;
+  useEffect(() => {
+    const checkMobileView = () => {
+      setIsMobileView(window.innerWidth <= 769);
+    };
+
+    checkMobileView();
+    window.addEventListener("resize", checkMobileView);
+
+    return () => {
+      window.removeEventListener("resize", checkMobileView);
+    };
+  }, []);
+
+  const drawerContent = (
+    <div>
+      <List>
+        {menuItems.map((item, index) => (
+          <ListItem key={index} onClick={() => navigate(item.link)} style={{ cursor: "pointer" }}>
+            <ListItemText primary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
 
   return (
-
     <>
       <AppBar position="static" sx={{ backgroundColor: "#10617a" }}>
         <Toolbar>
           <div className={`${styles.grow} ${styles.leftAlign}`}>
-            {mobileView ? (
-              <HamburgerMenu menuItems={menuItems} />
+          {isMobileView && location.pathname !== "/panel" ? (
+              <>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleDrawerOpen}
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Drawer
+                anchor="left"
+                open={openDrawer}
+                onClose={handleDrawerClose}
+              >
+                {drawerContent}
+              </Drawer>
+            </>
             ) : (
               menuItems.map((item, index) => (
                 <Link to={item.link} className={styles.navLink} key={index}>
